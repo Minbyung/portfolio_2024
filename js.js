1a6439f3b5e75178a3701589fec3ed4008b62fd2
Minbyung <mbmcybery@gmail.com>
2024-07-22 17:30:41 +0900

    사진 넣기전 최종

--- js.js
+++ js.js
@@ -1,29 +1,35 @@
 const sections = document.querySelectorAll('section');
 const navLi = document.querySelectorAll('.nav-bar ul li');
+const navBar = document.querySelector('.nav-bar');
 
 window.addEventListener('scroll', () => {
-    // Log the current scroll positions
-    console.log(window.scrollX, window.scrollY);
-    
     let current = '';
 
     sections.forEach(section => {
-        const sectionTop = section.offsetTop; // Get the distance of the section from the top of the page
-        const sectionHeight = section.offsetHeight; // Get the height of the section
-        // Check if the section is in the viewport
-        if (window.scrollY >= sectionTop - sectionHeight / 3 && window.scrollY < sectionTop + sectionHeight - sectionHeight / 3) {
-            current = section.getAttribute('id'); // Get the id of the current section
+        const sectionTop = section.offsetTop; // 섹션이 페이지 상단에서부터 떨어진 거리
+        const sectionHeight = section.offsetHeight; // 섹션의 높이
+        if (window.scrollY >= sectionTop - sectionHeight / 3 && window.scrollY < sectionTop + 953) {
+            current = section.getAttribute('id'); 
         }
     });
 
-    // Log the current section id
-    console.log('Current Section:', current);
-
     navLi.forEach(li => {
-        li.classList.remove('active'); // Remove the active class from all navigation items
-        // Add the active class to the navigation item corresponding to the current section
+        li.classList.remove('active'); 
         if (li.querySelector('a').getAttribute('href').includes(current)) {
             li.classList.add('active');
         }
     });
 });
+
+window.addEventListener('load', function() {
+    document.getElementById('home').scrollIntoView();
+});
+
+function changeImage(src, button) {
+    document.getElementById('mainImage').src = src;
+
+    const buttons = document.querySelectorAll('.image-buttons button');
+    buttons.forEach(btn => btn.classList.remove('active'));
+    
+    button.classList.add('active');
+}
